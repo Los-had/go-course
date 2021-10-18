f8064cfc910e0d1e1e3ce611fc648b42d105e0a1
Los-had <githubtestefinal@gmail.com>
2021-10-18 16:49:21 -0300

    Input

--- src/basic/input/input.go
+++ src/basic/input/input.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strconv"
+)
+
 func main() {
-	return
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Type the year that you was born: ")
+	scanner.Scan()
+	input, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	fmt.Println("You will be", 2022 - input, "years in 2022")
 }
