5520a6d5661c9d23b3ba798287ffaa0aea2ae3e4
Los-had <githubtestefinal@gmail.com>
2021-10-18 16:59:10 -0300

    If, else and else if

--- src/basic/conditional/if_else.go
+++ src/basic/conditional/if_else.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"strconv"
+	"os"
+	"fmt"
+	"bufio"
+)
+
 func main() {
-	return
+	fmt.Println("Welcome!")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Type your age: ")
+	scanner.Scan()
+	input, err := strconv.ParseInt(scanner.Text(), 10, 64)
+
+	if err != nil {
+		fmt.Println(err)
+		
+		return
+	}
+
+	if input < 18 && input > 14 {
+		fmt.Println("Can ride with a parent.")
+	} else if input < 14 {
+		fmt.Println("Can't ride")
+	} else {
+		fmt.Println("You can ride alone")
+	}
 }
