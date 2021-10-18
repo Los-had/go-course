62bd2db6adab507ec0dce41f8c8bcbdc1fe88896
Los-had <githubtestefinal@gmail.com>
2021-10-18 17:40:57 -0300

    Arrays

--- src/basic/arrays/arrays.go
+++ src/basic/arrays/arrays.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	return
+	var arr [5]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	arr[3] = 4
+	arr[4] = 5
+	arr2 := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(arr)
+	fmt.Println(arr2)
 }
