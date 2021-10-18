8a11c90c788cbc303f5ba3c4b94950fd66e4f8f6
Los-had <githubtestefinal@gmail.com>
2021-10-18 16:30:56 -0300

    Variables

--- src/basic/variables/variables.go
+++ src/basic/variables/variables.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	
+	var name string = "Miguel"
+	name2 := "Miguel"
+	var age int = 16
+	age2 := 16
+	var hireable bool = false
+	hireable2 := false
+
+	fmt.Println(name, name2)
+	fmt.Println(age, age2)
+	fmt.Println(hireable, hireable2)
 }
