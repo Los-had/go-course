3cc87b6f016e8a129253c02aab8a3500767942ad
Los-had <githubtestefinal@gmail.com>
2021-10-18 17:16:54 -0300

    For - loops 1

--- src/basic/loops/for.go
+++ src/basic/loops/for.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	return
+	for x := 0; x < 10; x++ {
+		fmt.Println(x)
+	}
+	fmt.Println("-------------------------------------------")
+	// other way to use for loops
+	y := 0
+	for y <= 15 {
+		fmt.Println(y)
+		y++
+	}
+	fmt.Println("-------------------------------------------")
+	// prints the divisible by 3 numbers in range of 0 to 1000
+	for z := 1; z <= 1000; z++ {
+		if z % 3 == 0 {
+			fmt.Println(z)
+		} else {
+			continue
+		}
+	} 
 }
